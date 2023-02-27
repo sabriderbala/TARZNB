1abff07ab79b7e87e24e4f01b00130c4c05bb01e
Ana Candellier <ana.candellier@gmail.com>
2023-02-27 17:15:59 +0100

    new booking

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+user = User.create!({
+  last_name: "le TA",
+  first_name: "Baptiste",
+  email: "baptiste.le-ta@lewagon.com",
+  password: "123456"
+})
+
+Caban.create!({
+  name: "Cabane 1",
+  address: "68 avenue Parmentier",
+  price: 120,
+  description: "Description de la cabane 1",
+  user_id: user.id
+})
