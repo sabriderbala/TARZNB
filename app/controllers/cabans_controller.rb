a7f3486bcc98d9fe566012093b4b650518c45a12
Ana Candellier <ana.candellier@gmail.com>
2023-02-28 16:25:32 +0100

    authorization changes

--- app/controllers/cabans_controller.rb
+++ app/controllers/cabans_controller.rb
@@ -1,19 +1,23 @@
 class CabansController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
   def index
-    @cabans = Caban.all
+    @cabans = policy_scope(Caban)
   end
 
   def show
     @caban = Caban.find(params[:id])
+    authorize @caban
   end
 
   def new
     @caban = Caban.new
+    authorize @caban
   end
 
   def create
     @caban = Caban.new(caban_params)
     @caban.user = current_user
+    authorize @caban
     if @caban.save
       redirect_to caban_path(@caban)
     else
@@ -24,16 +28,19 @@ class CabansController < ApplicationController
   def destroy
       @caban = Caban.find(params[:id])
       @caban.destroy
+      authorize @caban
       redirect_to dashboard_path, status: :see_other
   end
 
   def edit
     @caban = Caban.find(params[:id])
+    authorize @caban
   end
 
   def update
     @caban = Caban.find(params[:id])
     @caban.update(caban_params)
+    authorize @caban
     redirect_to caban_path(@caban), status: :see_other
   end
 
