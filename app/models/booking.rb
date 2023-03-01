2546c31c1a9091bb53a99810b4d89957b872be72
Noemie Nataf <noemienataf@gmail.com>
2023-03-01 16:06:20 +0100

    date pickr

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,4 +3,6 @@ class Booking < ApplicationRecord
   belongs_to :caban
 
   validates :status, inclusion: { in: %w(pending accepted declined) }
+  validates :end_date, comparison: {greater_than: :start_date}
+
 end
