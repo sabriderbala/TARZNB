42303e064625ce312ffbe739728ac1a44a9f78e9
Ana Candellier <ana.candellier@gmail.com>
2023-03-02 11:23:33 +0100

    search cabans

--- app/models/caban.rb
+++ app/models/caban.rb
@@ -1,9 +1,17 @@
 class Caban < ApplicationRecord
   include CabanConcern
+  include PgSearch::Model
+  pg_search_scope :global_search,
+  against: [ :name, :description, :address ],
+  using: {
+    tsearch: { prefix: true }
+  }
 
   belongs_to :user
   has_many :bookings
   has_many_attached :photos, dependent: :purge
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+
+
 end
